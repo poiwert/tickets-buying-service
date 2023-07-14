79124d5fe4da62804c0409391ebf7c244f2c025b
Bogdana <n22-b.danyliuk@nubip.edu.ua>
2023-07-14 12:29:54 +0300

    title  'filmname' changed to title  'film_name'

--- film_info.c
+++ film_info.c
@@ -1,11 +1,11 @@
 int film_info(int filmname) {
-    if (filmname == 1) {
+    if (film_name == 1) {
         printf("IMF agent Ethan Hunt's new mission is to retrieve half of a key from his ally Ilsa Faust, on whom IMF has placed a bounty. He travels to the Empty Quarter in the Arabian Desert, briefly reunites with Ilsa and tells her to lay low. Back in the US, Ethan infiltrates a meeting of the Community, where officials of various intelligence agencies, including former IMF director Eugene Kittridge and DNI Denlinger, discuss an experimental AI called the Entity. Originally designed to sabotage digital systems, the Entity went rogue, expanded to potential sentience, and infiltrated all major defense and military systems and intelligence networks. Major powers are racing both to prevent sabotage and to gain control of the Entity.\n");
     }
-     else if (filmname == 2) {
+     else if (film_name == 2) {
         printf("The film's screenplay is based on the book 'American Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer' written by Kai Bird and Martin Sherwin, which received the Pulitzer Prize for Biography or Autobiography. The main character is an American physicist who, under his leadership, developed the atomic bomb during World War II.\n");
     }
-     else if (filmname == 3) {
+     else if (film_name == 3) {
         printf("Family comedy, telling the story of the world's most famous doll - what if Barbie were a real girl living among us? Too imperfect for the perfect world of Barbiedom, she ends up being banished and encounters the harsh reality of the human world.\n");
     }
 }
