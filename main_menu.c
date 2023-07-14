5e050a0b02aee1886e4ac906093e9cbee603b0e3
poiwert <max080405@ukr.net>
2023-07-14 15:52:00 +0300

    Fixed a problem with references to variables and functions defined in the "categories.h" and "menu_styles.h" files.
    The categories.c file was added to define the categories and
    num_categories variables that contain information about menu
    categories. These variables are used in main_menu.c to display the
    available categories in the menu.

--- main_menu.c
+++ main_menu.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include "categories.h"
+#include "menu_styles.h"
 
-int account_info(){}
-int buy(){}
+extern const char* categories[];
+extern const int num_categories;
 
-void print_menu_styles() {
-    printf(" Choose menu section\n");
-    printf("__________________________________\n");
-    printf("| Account (1) Film catalog (2) |\n");
-    printf("__________________________________\n");
+int account_info() {}
+int buy() {}
+
+void display_categories() {
+    for (int i = 0; i < num_categories; i++) {
+        printf("(%d) %s\n", i + 1, categories[i]);
+    }
 }
 
-int main(){
+int main() {
     while (1) {
         int menu_section, info, purchase;
         print_menu_styles();
-        for (int i = 0; i < num_categories; i++) {
-            printf("(%d) %s\n", i + 1, categories[i]);
-        }
+        display_categories();
         scanf("%d", &menu_section);
-        
+
         switch (menu_section) {
             case 1:
                 info = account_info();
--- menu_styles.h
+++ menu_styles.h
@@ -1,6 +1,13 @@
 #ifndef MENU_STYLES_H
 #define MENU_STYLES_H
 
-void print_menu_styles();
+#include <stdio.h>
+
+void print_menu_styles() {
+    printf(" Choose menu section\n");
+    printf("__________________________________\n");
+    printf("| Account (1) Film catalog (2) |\n");
+    printf("__________________________________\n");
+}
 
 #endif
