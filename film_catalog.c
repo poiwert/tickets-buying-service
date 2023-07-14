ac00dfa153e15171f58b3796d410495276be67c4
poiwert <max080405@ukr.net>
2023-07-14 12:32:28 +0300

    General fixes
    Added a global variable for further use in other functions
    also changed the name of the variable (purchase)
    to a more appropriate one (info)
    fixed typo

--- film_catalog.c
+++ film_catalog.c
@@ -1,27 +1,33 @@
+// Глобальна змінна
+int film_name;
+
 int film_catalog() {        
-    int buy_tickets_menu_section, purchase, return_function;
+    int buy_tickets_menu_section, info, return_function;
         printf("\tChoose a film \n");
-        printf("\tMission impossible: Reckoning. Part One (1) \n");
-        printf("\tOppenheimer (2)  \n");
-        printf("\tHeroes of the golden masks (3)  \n");
+        printf("\tMission impossible: Reckoning. Part One (1\n");
+        printf("\tOppenheimer (2)\n");
+        printf("\tBarby (3)\n");
         printf("\tExit (0)\n");
         scanf("%d", &buy_tickets_menu_section);
         
         switch (buy_tickets_menu_section) {
             case 1:
-                printf("You have chosen: Kyky kaka\n");
-                
+                printf("You have chosen: Mission impossible: Reckoning. Part One\n");
+                film_name = 1;
+                info=film_info(film_name);
                 break;
             case 2:
-                printf("You have chosen: Topol\n");
-                
+                printf("You have chosen: Oppenheimer\n");
+                film_name = 2;
+                info=film_info(film_name);
                 break;
             case 3:
-                printf("You have chosen: RTR\n");
-
+                printf("You have chosen: Barby\n");
+                film_name = 3;
+                info=film_info(film_name);
                 break;
             case 0:
-                printf("Exit to film catalog\n");
+                printf("Exit to main menu\n");
                 return_function = main();
                 break; 
             default:
