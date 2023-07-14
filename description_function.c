94aaf08815dc9dc24aa7898bb90895cf32e8bbbf
poiwert <max080405@ukr.net>
2023-07-14 17:35:23 +0300

    Added functionality

--- description_function.c
+++ description_function.c
@@ -3,6 +3,17 @@ int quantity_of_tickets = 0,money_spended_summary = 0;
 int description_function() {
     while (1)
     {
-        /* code */
+        printf("\tDescription\n");
+        if (quantity_of_tickets == 0) {
+            printf("Watch something, we need some info\n");
+            printf("__________________________________\n");
+            printf("\n");
+        } else {
+            printf("Overall you watched %d movies! All this cost you %d UAH\n", quantity_of_tickets, money_spended_summary);
+            printf("__________________________________\n");
+            printf("\n");
+            
+        }
+        return account_info();
     }
 }
