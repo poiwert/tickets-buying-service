b5ac26395a9cbfb0bdce6ccab50f33c894497620
poiwert <max080405@ukr.net>
2023-07-14 16:43:03 +0300

    Added menu and loop

--- account_info.c
+++ account_info.c
@@ -1,3 +1,25 @@
 int account_info() {
-    
+        while (1) {
+        printf("(Enter '0' to go back to the main menu)\n");
+        int account_info_menu_section, return_function, desc;
+        printf("Pick info that you want to see\n");
+        printf("Description(1) Active session(2)\n");
+        scanf("%d", &account_info_menu_section);
+        
+        switch (account_info_menu_section) {
+            case 0:
+                printf("Exit to main menu");
+                return_function = main();
+                break;
+            case 1:
+                desc = description_function();
+                break;
+            case 2:
+                printf("Active session\n");
+                break;
+            default:
+                printf("Input another key\n");
+                break;
+        }
+    }
 }
