245255bb38912572e25844f73cb8e04f6a9008c6
poiwert <max080405@ukr.net>
2023-07-14 11:05:32 +0300

    Added variables and menu sections using "switch case"

--- main_menu.c
+++ main_menu.c
@@ -2,5 +2,20 @@
 #include <string.h>
 
 int main(){
-
+    int menu_section, info, buy;
+    printf("Choose menu section\n");
+    printf("\t Account(1)\t Buy(2) \n");
+    scanf("%d", &menu_section);
+    
+    switch (menu_section) {
+        case 1:
+            info = account_info();
+            break;
+        case 2:
+            buy = buy_tickets();
+            break;
+        default:
+            printf("Input another key\n");
+            break;
+        }
 }
