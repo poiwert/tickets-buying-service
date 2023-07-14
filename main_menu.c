29a48655b07fcf2cc1633a0a24a2dd44b076cc59
poiwert <max080405@ukr.net>
2023-07-14 15:10:21 +0300

    Refactor Main Code
    In this code, the unnecessary declared variable film_catalog()
    was removed, as its name was changed to buy().
    Thus, the updated code corresponds to the changed function names
    and ensures consistency with the variable declaration.

--- main_menu.c
+++ main_menu.c
@@ -1,28 +1,27 @@
+// main.c
 #include <stdio.h>
 #include <string.h>
+#include "menu_styles.h"
 
 int account_info(){}
-int film_catalog(){}
+int buy(){}
 
 int main(){
     while (1) {
-    int menu_section, info, buy;
-    printf("      Choose menu section\n");
-    printf("__________________________________\n");
-    printf("| Account (1)   Film catalog (2) |\n");
-    printf("__________________________________\n");
-    scanf("%d", &menu_section);
-    
-    switch (menu_section) {
-        case 1:
-            info = account_info();
-            break;
-        case 2:
-            buy = film_catalog();
-            break;
-        default:
-            printf("Input another key\n");
-            break;
+        int menu_section, info, purchase;
+        print_menu_styles();
+        scanf("%d", &menu_section);
+        
+        switch (menu_section) {
+            case 1:
+                info = account_info();
+                break;
+            case 2:
+                purchase = buy();
+                break;
+            default:
+                printf("Input another key\n");
+                break;
         }
     }
     return 0;
