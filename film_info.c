92b1bca432f51acbc5cacf29a6af7131ea8a520c
Bogdana <n22-b.danyliuk@nubip.edu.ua>
2023-07-14 12:14:30 +0300

    Added description to film 1

---
+++ film_info.c
@@ -0,0 +1,5 @@
+int film_info(int filmname) {
+    if (filmname == 1) {
+        printf("IMF agent Ethan Hunt's new mission is to retrieve half of a key from his ally Ilsa Faust, on whom IMF has placed a bounty. He travels to the Empty Quarter in the Arabian Desert, briefly reunites with Ilsa and tells her to lay low. Back in the US, Ethan infiltrates a meeting of the Community, where officials of various intelligence agencies, including former IMF director Eugene Kittridge and DNI Denlinger, discuss an experimental AI called the Entity. Originally designed to sabotage digital systems, the Entity went rogue, expanded to potential sentience, and infiltrated all major defense and military systems and intelligence networks. Major powers are racing both to prevent sabotage and to gain control of the Entity.\n");
+    }
+}
