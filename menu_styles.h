3908a9c95b14c8dc87f9f41041b5f20f41a1bdd0
poiwert <max080405@ukr.net>
2023-07-14 15:04:30 +0300

    Extract Menu Styles to Separate File
    A new file "menu_styles.h" is created, where the print_menu_styles()
    function is declared, which is responsible for displaying menu styles.
    The main file "main.c" includes this file using the
     #include "menu_styles.h" directive.
    The print_menu_styles() function is called in the main loop of the
    program to display the menu styles before the user selects.

---
+++ menu_styles.h
@@ -0,0 +1,41 @@
+// menu_styles.h
+#ifndef MENU_STYLES_H
+#define MENU_STYLES_H
+
+void print_menu_styles() {
+    printf(" Choose menu section\n");
+    printf("__________________________________\n");
+    printf("| Account (1) Film catalog (2) |\n");
+    printf("__________________________________\n");
+}
+
+#endif
+
+// main.c
+#include <stdio.h>
+#include <string.h>
+#include "menu_styles.h"
+
+int account_info(){}
+int film_catalog(){}
+
+int main(){
+    while (1) {
+        int menu_section, info, buy;
+        print_menu_styles();
+        scanf("%d", &menu_section);
+        
+        switch (menu_section) {
+            case 1:
+                info = account_info();
+                break;
+            case 2:
+                buy = film_catalog();
+                break;
+            default:
+                printf("Input another key\n");
+                break;
+        }
+    }
+    return 0;
+}
