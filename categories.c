f80bb545664cb614d2ac33de19965930e703b650
Max Ostrovskyi <138432671+poiwert@users.noreply.github.com>
2023-07-15 12:30:38 +0300

    Delete categories.c
    Delete categories.c to avoid merge conflict

--- categories.c
+++
@@ -1,8 +0,0 @@
-#include "categories.h"
-
-const char* categories[] = {
-    "Information about account",
-    "Film catalog and buy section"
-};
-
-const int num_categories = sizeof(categories) / sizeof(categories[0]);
