ac20bef8301f26fd39c5f3ce576993630c705cc4
poiwert <max080405@ukr.net>
2023-07-15 13:22:07 +0300

    Added struct functionality? and sort of UI

--- film_info.c
+++ film_info.c
@@ -1,28 +1,24 @@
 
-    int cinema_info(int filmname) {
+    void cinema_info(struct Movie movie) {
         while(1){
-    if (film_name == 1) {
-        printf("IMF agent Ethan Hunt's new mission is to retrieve half of a key from his ally Ilsa Faust, on whom IMF has placed a bounty. He travels to the Empty Quarter in the Arabian Desert, briefly reunites with Ilsa and tells her to lay low. Back in the US, Ethan infiltrates a meeting of the Community, where officials of various intelligence agencies, including former IMF director Eugene Kittridge and DNI Denlinger, discuss an experimental AI called the Entity. Originally designed to sabotage digital systems, the Entity went rogue, expanded to potential sentience, and infiltrated all major defense and military systems and intelligence networks. Major powers are racing both to prevent sabotage and to gain control of the Entity.\n");
-    }
-     else if (film_name == 2) {
-        printf("The film's screenplay is based on the book 'American Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer' written by Kai Bird and Martin Sherwin, which received the Pulitzer Prize for Biography or Autobiography. The main character is an American physicist who, under his leadership, developed the atomic bomb during World War II.\n");
-    }
-     else if (film_name == 3) {
-        printf("Family comedy, telling the story of the world's most famous doll - what if Barbie were a real girl living among us? Too imperfect for the perfect world of Barbiedom, she ends up being banished and encounters the harsh reality of the human world.\n");
-    }
+        printf("__________________________________________________________\n");
+        printf("Film Name: %s\n", movie.name);
+        printf("__________________________________________________________\n");
+        printf("Description: %s\n", movie.description);
+        printf("__________________________________________________________\n");
 
-    int buy_tickets_choice;
-    printf("Would you like to buy tickets for this film? (1 - Yes, 0 - No): ");
-    scanf("%d", &buy_tickets_choice);
-    if (buy_tickets_choice == 1) {
-        purchasing_function();
-    }
-    if(buy_tickets_choice == 0){
-        film_catalog();
-      }
-        else{
-            printf("Input another key\n");
-        
+        int buy_tickets_choice;
+        printf("Would you like to buy tickets for this film? (1 - Yes, 0 - No): ");
+        scanf("%d", &buy_tickets_choice);
+        if (buy_tickets_choice == 1) {
+            purchasing_function();
         }
+        if(buy_tickets_choice == 0){
+            return;
+        }
+            else{
+            printf("Input another key\n");
+        }
+        printf("__________________________________________________________\n");
     }
 }
