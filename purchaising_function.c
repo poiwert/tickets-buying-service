908cf9c80138b29f1b56b2b062bcfc31dff6b638
poiwert <max080405@ukr.net>
2023-07-16 18:50:59 +0300

    Added purchasing_function_display_menu function

--- purchaising_function.c
+++ purchaising_function.c
@@ -1,3 +1,22 @@
+void purchasing_function_display_menu(){
+        printf("__________________________________\n");
+        printf("\t  |               |\n");
+        printf("\t  | Choose a time |\n");
+        printf("\t  |               |\n");
+        printf("__________________________________\n");
+        printf("\t| 10:00, 120 UAH (1) |\n");
+        printf("__________________________________\n");
+        printf("\t| 12:00, 140 UAH (2) |\n");
+        printf("__________________________________\n");
+        printf("\t| 16:00, 160 UAH (3) |\n");
+        printf("__________________________________\n");
+        printf("\t| 18:00, 180 UAH (4) |\n");
+        printf("__________________________________\n");
+        printf("\t| 20:00, 180 UAH (5) |\n");
+        printf("__________________________________\n");
+        printf("\t      Exit (0)\n");
+} 
+
 int purchasing_function() {
-    
+    purchasing_function_display_menu();
 }
