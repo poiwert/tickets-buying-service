5e426015c9096dbf96e379c39d3c17c9de2d90fa
poiwert <max080405@ukr.net>
2023-07-19 15:42:11 +0300

    Fixed typo

--- show_history.c
+++ show_history.c
@@ -5,7 +5,7 @@ void show_history(){
     }
 
     else{
-        printf("Your account history\nFor more convenient viewing of statistics  you can watch it in file %s in program ditrectory\n",history_filename);
+        printf("Your account history\n For more convenient viewing of statistics  you can watch it in file %s in program ditrectory\n",history_filename);
         printf("______________________________________\n");
         FILE *file = fopen(history_filename, "r");
         if (file == NULL){
