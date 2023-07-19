98bd72b88ac2d6aa416970521bc504044815b361
poiwert <max080405@ukr.net>
2023-07-19 12:39:44 +0300

    Added constant 'const char* history_filename="History.txt";'
    The added function, `check_duplicate_tickets`, checks for duplicate
    tickets for the current movie session.
    By reading data from the history file.If the file does not exist,
    it creates a new file. After successfully creating the file,
    the function immediately calls `purchase_accept`
    (because there is no data to compare) to continue the ticketing process.
    
    The function reads the file line by line and extracts ticket data from
    each line. The read data from each line is compared with the data of the
    current ticket. If a duplicate ticket is found
    (compared to the following data:
    film_id, date, time, row, place, localization) a message about the found
    duplicate is displayed, and the `purchasing_function` is called
    to change the ticket data.

--- purchase_accept.c
+++ purchase_accept.c
@@ -1,6 +1,70 @@
+const char* history_filename="History.txt";
 
+void check_duplicate_tickets(struct DateTime* dt, struct ActiveSession* active_session, struct Movie movie){
 
-void check_duplicate_tickets(){
+    FILE* file = fopen(history_filename, "ab+");
+
+    if (file == NULL) {
+
+        file = fopen(history_filename, "wb");
+
+        if (file == NULL) {
+            printf("File creation error\n");
+        }
+
+        fclose(file);
+        purchase_accept(dt, active_session, movie);
+    }
+    
+    char line[256];
+    char localization_from_history[100];
+    int film_id_from_history;
+    int date_from_history;
+    int time_from_history;
+    int row_from_history;
+    int seat_from_history;
+    int film_localization_id_from_history;
+    char name_from_history[100] = ""; 
+
+    while (fgets(line, sizeof(line), file)) {
+
+        // Extract the ticket data from each line in the history file
+        sscanf(line, " Name: %[^\n]", name_from_history);
+        sscanf(line, " Date: %d", &date_from_history);
+        sscanf(line, " Time: %d", &time_from_history);
+        sscanf(line, " Row: %d", &row_from_history);
+        sscanf(line, " Seat: %d", &seat_from_history);
+        sscanf(line, " Localization: %s", localization_from_history);
+
+        if (strcmp(localization_from_history, "Ukrainian") == 0) {
+            film_localization_id_from_history = 0;
+        } else if (strcmp(localization_from_history, "English") == 0) {
+            film_localization_id_from_history = 1;
+        }
+        
+        if (strncmp(name_from_history, "Mission impossible: Reckoning. Part One\0", 39) == 0) {
+            film_id_from_history = 1; 
+        } else if (strncmp(name_from_history, "Oppenheimer\0",11) == 0) {
+            film_id_from_history = 2; 
+        } else if (strncmp(name_from_history, "Barby\0",5) == 0) {
+            film_id_from_history = 3; 
+        }
+    }
+
+        // Compare the extracted data with the data of the current ticket
+        if (film_id_from_history == buy_tickets_menu_section &&
+        date_from_history == date_session &&
+        time_from_history == session_time &&
+        row_from_history == row &&
+        seat_from_history == seat &&
+        film_localization_id_from_history == film_localization_id) {
+            printf("Duplicate ticket found. Change ticket data\n");
+            fclose(file);
+            purchasing_function(movie);
+        }
+
+    fclose(file);
+    return;
     
 }
 
