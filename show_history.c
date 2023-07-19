8e05329466d2a3a2f62b74b0744c423eb72c4220
poiwert <max080405@ukr.net>
2023-07-19 20:51:04 +0300

    Fixed merge conflict

---
+++ show_history.c
@@ -0,0 +1,26 @@
+void show_history(){
+
+    if(quantity_of_tickets==0){
+        printf("History is empty\n");
+    }
+
+    else{
+        printf("Your account history\n For more convenient viewing of statistics  you can watch it in file %s in program ditrectory\n",history_filename);
+        printf("______________________________________\n");
+        FILE *file = fopen(history_filename, "r");
+        if (file == NULL){
+            printf("File open error\n");
+            return;
+        }
+
+        char buffer[256];
+        while (fgets(buffer, sizeof(buffer), file) != NULL){
+            printf("%s", buffer);
+        }
+
+        fclose(file);
+        printf("______________________________________\n");
+        printf("\n");
+    }
+
+}
