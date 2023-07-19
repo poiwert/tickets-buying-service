315ec8436dac369137430bd7dce8b7f16ed9191a
poiwert <max080405@ukr.net>
2023-07-19 15:31:09 +0300

    Added functionality to show history, by reading history filt and print it in console

--- show_history.c
+++ show_history.c
@@ -1,3 +1,26 @@
 void show_history(){
+
+    if(quantity_of_tickets==0){
+        printf("History is empty\n");
+    }
+
+    else{
+        printf("Your account history\n");
+        printf("______________________________________\n");
+        FILE *file = fopen(history_filename, "r");
+        if (file == NULL){
+            printf("File open error\n");
+            return;
+        }
+
+        char buffer[256];
+        while (fgets(buffer, sizeof(buffer), file) != NULL){
+            printf("%s", buffer);
+        }
+
+        fclose(file);
+        printf("______________________________________\n");
+        printf("\n");
+    }
     
 }
