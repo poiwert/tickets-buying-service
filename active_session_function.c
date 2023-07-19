ff8947a0e6ee939854111230269283e3f3526a31
poiwert <max080405@ukr.net>
2023-07-19 16:39:39 +0300

    Function that displays data on the last purchased ticket (active session)

--- active_session_function.c
+++ active_session_function.c
@@ -1,3 +1,19 @@
-int active_session_function(){
+int active_session_function(struct ActiveSession* active_session, struct DateTime* dt){
+    
+    if (quantity_of_tickets==0){
+        printf("You have no active sessions\n");
+    }
+
+    else{
+
+        printf("Name: %s\n",active_session->movie_name);
+        printf("Time: %d:00\n",active_session->time);
+        printf("Date: %d-%02d-%02d\n",active_session->date, dt->month, dt->year);
+        printf("Row: %d\n",active_session->row);
+        printf("Seat: %d\n",active_session->seat);
+        printf("Localization: %s\n", ((active_session->film_localization_id == 0) ? "Ukrainian" : "English"));
+        calculate_expired_session(active_session,dt);
+        free(dt);
+    }
     
 }
