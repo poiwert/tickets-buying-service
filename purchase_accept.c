c75fbc1a344218291ee5a315507a273d9f7116c5
poiwert <max080405@ukr.net>
2023-07-19 12:19:25 +0300

    The `purchase_accept` function is responsible for processing the purchase of a movie ticket.
    Calls the `check_duplicate_tickets` function, which checks for duplicate
    tickets for a given session.
    This is important to prevent repeated purchases of tickets for the same
    session.
    
    Displays information about the movie for which the user is buying
    a ticket.
    
    If the user confirms the purchase (`purchase_confirm == 1`),
    the data about the active session is saved in the `active_session`
    structure, the total amount of money and the number of purchased tickets
    are additionally calculated and saved.
    
    If the user cancels the purchase (`purchase_confirm == 0`),
    a corresponding message is displayed.And returns the user to the movie
    catalog

--- purchase_accept.c
+++ purchase_accept.c
@@ -1,3 +1,54 @@
-int purchase_accept(){
+int purchase_accept(struct DateTime* dt, struct ActiveSession* active_session, struct Movie movie){
+
+    check_duplicate_tickets(dt,active_session,movie);
+
+    int purchase_confirm;
+
+    printf("Movie name: %s\n", movie.name); 
+    printf("Date session: %d-%02d-%02d\n",date_session,dt->month,dt->year);
+    printf("Session time: %d:00\n",session_time);
+    printf("Seat: %d\n",seat);
+    printf("Row: %d\n",row);
+    printf("Localization: %s\n", (film_localization_id == 0) ? "Ukrainian" : "English");
+
+    if(popcorn_store_entry==1){
+        printf("You spent %d UAH in popcorn store\n",popcorn_menu);
+    }
+
+    printf("Cost: %d UAH\n", money_spended);
+
+    while (1){
+
+        printf("Confirm purchase? Yes(1), No(0): ");
+        scanf("%d",&purchase_confirm);
+
+        if (purchase_confirm==1){
+
+            strcpy(active_session->movie_name, movie.name); 
+            active_session->date = date_session;
+            active_session->time = session_time;
+            active_session->row = row;
+            active_session->seat = seat;
+            active_session->film_localization_id = film_localization_id;
+            money_spended_summary+=money_spended;
+            quantity_of_tickets++;
+            printf("Purchase complete! Now you can check it in acctive session\n");
+            ticket_creator(dt,active_session,movie);
+            film_catalog(movie);
+            break;
+
+        }
+
+        if (purchase_confirm==0){
+
+            printf("Purchaising denied!\n");
+            film_catalog(movie);
+            break;
+
+        }
+
+        else printf("Invalid input\n"); 
+
+    }
     
 }
