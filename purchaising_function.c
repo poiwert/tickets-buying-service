ff8f5e385d8d49646609ffe914d547a11bff817d
poiwert <max080405@ukr.net>
2023-07-19 11:24:56 +0300

    Changed the name of the method to a more appropriate one and added пробіли

--- purchaising_function.c
+++ purchaising_function.c
@@ -53,54 +53,66 @@ int calculate_money_from_popcorn_store(struct DateTime* dt, struct ActiveSession
 }
 
 int popcorn_store(struct DateTime* dt, struct ActiveSession* active_session, struct Movie movie){
+
     while (1){
-            printf("Popcorn and drinks (1) +150 UAH\n");
-            printf("Only popcorn (2) +120 UAH\n");
-            printf("Only drinks (3) +30 UAH\n");
-            printf("Neither (0)\n");
-            scanf("%d", &popcorn_menu);
-                switch (popcorn_menu) {
-                    case 3: 
-                        printf("Only drinks\n");
-                        calculate_money_from_popcorn_store(dt,active_session,movie);
-                    return;
-                    case 2: 
-                        printf("Only popcorn\n");
-                        calculate_money_from_popcorn_store(dt,active_session,movie);
-                    return;      
-                    case 1: 
-                        printf("Popcorn and drinks!\n");
-                        calculate_money_from_popcorn_store(dt,active_session,movie);
-                    return;
-                    case 0: 
-                        printf("Neither\n");
-                        popcorn_menu = 0;
-                        purchase_accept(dt,active_session,movie);
-                    break;
-                    default: printf("Invalid input"); break;
-                }
+        printf("Popcorn and drinks (1) +150 UAH\n");
+        printf("Only popcorn (2) +120 UAH\n");
+        printf("Only drinks (3) +30 UAH\n");
+        printf("Neither (0)\n");
+        scanf("%d", &popcorn_menu);
+
+            switch (popcorn_menu) {
+
+                case 3: 
+                    printf("Only drinks\n");
+                    calculate_money_from_popcorn_store(dt,active_session,movie);
+                return;
+
+                case 2: 
+                    printf("Only popcorn\n");
+                    calculate_money_from_popcorn_store(dt,active_session,movie);
+                return;    
+
+                case 1: 
+                    printf("Popcorn and drinks!\n");
+                    calculate_money_from_popcorn_store(dt,active_session,movie);
+                return;
+
+                case 0: 
+                    printf("Neither\n");
+                    popcorn_menu = 0;
+                    purchase_accept(dt,active_session,movie);
+                break;
+
+                default: printf("Invalid input"); break;
             }
+        }
+
 }
 
 int film_localization_selection(struct DateTime* dt, struct ActiveSession* active_session, struct Movie movie) {
+    
     while (1){
         printf("Choose the localization of the film:\n");
         printf("0 - Ukrainian\n");
         printf("1 - English\n");
         scanf("%d", &film_localization_id);
         printf("Visit popcorn store? Yes (1), No (0):");
-            scanf("%d",&popcorn_store_entry);
+        scanf("%d",&popcorn_store_entry);
+
         if (popcorn_store_entry==1)
                 popcorn_store(dt,active_session, movie);
+
         if (popcorn_store_entry==0)
                 purchase_accept(dt, active_session,movie);
-        if (quantity_of_tickets>0)
-                purchase_accept(dt, active_session,movie);
+
         else printf("Invalid input\n");   
     }
+
 }
 
 int seat_and_row_selection(struct DateTime* dt, struct ActiveSession* active_session, struct Movie movie){
+
     while (1) {
         printf("Choose a row\n");
         printf("You can choose from '1' to '%d'\n", AMOUNT_OF_ROWS);
@@ -126,35 +138,40 @@ int seat_and_row_selection(struct DateTime* dt, struct ActiveSession* active_ses
             printf("There is no such a seat\nTry a different number\n");
         }
     }
+
 }
 
 void today_time_check(struct DateTime* dt, struct Movie movie){
+
     if (dt->day==date_session && dt->hour>=session_time){
         printf("Current time: %02d:%02d\n", dt->hour, dt->minute);
         printf("This time is expired\n");
         purchasing_function(movie);
     }
+
 }
 
 void date_selection_function(struct DateTime* dt, struct Movie movie){
     int month_end_day;
+
     start: // Label for restarting the cycle
     while(1){
+
         printf("Current date: %d-%02d-%02d\n", dt->day, dt->month, dt->year);
-        if (dt->month%2)
-        {
+
+        if (dt->month%2){
             month_end_day=30;
         }
-        if (dt->month==2)
-        {
+
+        if (dt->month==2){
             month_end_day=28;
         }
-        else
-        {
+
+        else{
             month_end_day=31;
         }
-        if(dt->year==2024||dt->year==2028||dt->year==2032)
-        {
+
+        if(dt->year==2024||dt->year==2028||dt->year==2032){
             if (dt->month==2)
             {
                 month_end_day=29;
@@ -165,40 +182,41 @@ void date_selection_function(struct DateTime* dt, struct Movie movie){
         printf("You can pick from %d to %d\n",dt->day,month_end_day);
         printf("(or press '0' if you want to pick other film)\n");
         scanf("%d", &date_session);
-        if (date_session==0)
-        {
+
+        if (date_session==0){
             film_catalog(movie);
         }
         
-        if (date_session<dt->day||date_session>month_end_day)
-        {
+        if (date_session<dt->day||date_session>month_end_day){
             printf("__________________________________________\n");
             printf("\tChoose another date\n");
             printf("__________________________________________\n");
             goto start; // Go to the beginning of the cycle
         }
-        if (date_session==dt->day)
-        {
-            if (dt->hour>=20)
-            {
+
+        if (date_session==dt->day){
+
+            if (dt->hour>=20){
                 printf("We have no more tickets for today\n");
             }
-            else
-            {
+
+            else{
                 printf("Chosen day is - %d\n",date_session);
                 return;
             }
             
         }
-        else
-        {
+
+        else{
             printf("Chosen day is - %d\n",date_session);
             return;
         }
+
     }
 }
 
-void current_date_and_time_calculate(struct DateTime* dt){
+void calculate_current_date_and_time(struct DateTime* dt){
+
     time_t currentTime;
     struct tm *localTime;
 
@@ -216,9 +234,11 @@ void current_date_and_time_calculate(struct DateTime* dt){
     // Get the time value
     dt->hour = localTime->tm_hour;
     dt->minute = localTime->tm_min;
+
 }
 
 void purchasing_function_display_menu(){
+
         printf("__________________________________\n");
         printf("\t  |               |\n");
         printf("\t  | Choose a time |\n");
@@ -235,35 +255,50 @@ void purchasing_function_display_menu(){
         printf("\t| 20:00, 180 UAH (5) |\n");
         printf("__________________________________\n");
         printf("\t      Exit (0)\n");
+
 } 
 
 int purchasing_function(struct Movie movie) {
+
     int purchasing_function_menu_section;
+
     struct ActiveSession* active_session = malloc(sizeof(struct ActiveSession));
+    
     strcpy(active_session->movie_name, movie.name); 
     active_session->date = date_session;
     active_session->time = session_time;
     active_session->row = row;
     active_session->seat = seat;
     active_session->film_localization_id = film_localization_id;
+
     struct DateTime* dt = malloc(sizeof(struct DateTime));
     purchasing_function_display_menu();
-        while (1)
+
+    while (1)
     {
+
         money_spended=0;
-        current_date_and_time_calculate(dt);
+        calculate_current_date_and_time(dt);
         date_selection_function(dt,movie);
         purchasing_function_display_menu();
         scanf("%d", &purchasing_function_menu_section);
+
         switch (purchasing_function_menu_section)
         {
         case 1: session_time=10; money_spended=120; today_time_check(dt,movie); seat_and_row_selection(dt,active_session,movie); free(dt); free(active_session); break;
+        
         case 2: session_time=12; money_spended=140; today_time_check(dt,movie); seat_and_row_selection(dt,active_session,movie); free(dt); free(active_session); break;
+        
         case 3: session_time=16; money_spended=160; today_time_check(dt,movie); seat_and_row_selection(dt,active_session,movie); free(dt); free(active_session); break;
+        
         case 4: session_time=18; money_spended=180; today_time_check(dt,movie); seat_and_row_selection(dt,active_session,movie); free(dt); free(active_session); break;
+        
         case 5: session_time=20; money_spended=180; today_time_check(dt,movie); seat_and_row_selection(dt,active_session,movie); free(dt); free(active_session); break;
+        
         case 0: free(dt); free(active_session); film_catalog(movie); break;    
+
         default: printf("Invalid input\n");
         }
+
     }
 }
