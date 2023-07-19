8afab6c1b4154a5adb34b991cc35e663dc13f22c
bobo <138432819+BogdanaDanyliuk@users.noreply.github.com>
2023-07-19 18:52:29 +0300

    Update film_catalog.c
    added new description  №2

--- film_catalog.c
+++ film_catalog.c
@@ -31,9 +31,13 @@ int film_catalog() {
                                    Cast: Tom Cruise (Mission: Impossible franchise), Pom Klementiev (Guardians of the Galaxy 2 franchise), Hayley Atwell (Agent Carter series), Vanessa Kirby (Fast and Furious: Hobbs and Shaw), Rebecca Ferguson (Mission: Impossible: Rogue Nation)\n
                                    Description: Ethan Hunt and the IMF team must track down a terrifying new weapon that threatens all of humanity if it falls into the wrong hands. With control of the future and the fate of the world at stake, a deadly race around the globe begins\n");
 
-    strcpy(movies[1].name, "Oppenheimer");
-    strcpy(movies[1].description, "The film's screenplay is based on the book 'American Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer' written by Kai Bird and Martin Sherwin, which received the Pulitzer Prize for Biography or Autobiography. The main character is an American physicist who, under his leadership, developed the atomic bomb during World War II.");
-
+    strcpy(movies[1].name, "Oppenheimer\0");
+    strcpy(movies[1].description, "Age: 16+\n
+                                   Director: Christopher Nolan (Inception, Dunkirk, The Shawshank Redemption, Interstellar)\n
+                                   Genre: Historical, Based on real events\n
+                                   Cast: Cillian Murphy (TV series 'Pointy Hats', 'Inception'), Emily Blunt (A Quiet Place dilogy), Robert Downey Jr. (Iron Man), Florence Pugh (Little Women, Black Widow), Matt Damon (The Outsiders, Ocean's Eleven)\n
+                                   Description: It is based on the 2005 biography American Prometheus by Kai Bird and Martin J. Sherwin about J. Robert Oppenheimer, a theoretical physicist who was pivotal in developing the first nuclear weapons as part of the Manhattan Project and thereby ushering in the Atomic Age\n");
+   
     strcpy(movies[2].name, "Barby");
     strcpy(movies[2].description, "Family comedy, telling the story of the world's most famous doll - what if Barbie were a real girl living among us? Too imperfect for the perfect world of Barbiedom, she ends up being banished and encounters the harsh reality of the human world.");
 
